952e341ca1aa868ae8a91975d386fc803b20d93b
V R Vishal <vrvishalmrfgit@gmail.com>
2023-08-20 20:52:39 +0530

    medai_app_commit

--- src/EditPost.js
+++ src/EditPost.js
@@ -15,7 +15,7 @@ const EditPost = () => {
       setEditTitle(post.title)
       setEditBody(post.body)
     }
-  }, [])
+  }, [post, setEditBody, setEditTitle])
   return (
     <main className="NewPost">
       { post &&
